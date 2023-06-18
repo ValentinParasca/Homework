4c47145ab96cc2107e1b7572f41a8f8ef63b0c38
ValentinParasca <136358621+ValentinParasca@users.noreply.github.com>
2023-06-18 21:58:39 +0300

    Delete MercedesCKlasse.java
    Remove unnecessary class.

--- src/main/java/Vehicles/MercedesCKlasse.java
+++
@@ -1,7 +0,0 @@
-package Vehicles;
-
-class MercCklasse extends Car {
-    public MercCklasse(float fuelTankSize, String fuelType, int gears, float consumptionPer100Km) {
-        super(fuelTankSize, fuelType, gears, consumptionPer100Km);
-    }
-}
